24d37b1da4c7cac9e54ff39f35ebdea4a094ed37
Tristan Joseph <tristan@Tristans-MacBook-Pro-2.local>
2018-04-12 19:51:12 +0200

    Athentication mixed with Jarryd's profile updates

--- Application/ticketsalad/client/scripts/controllers/profile.controller.js
+++ Application/ticketsalad/client/scripts/controllers/profile.controller.js
@@ -4,6 +4,19 @@ import { Controller } from 'angular-ecmascript/module-helpers';
 export default class ProfileCtrl extends Controller {
   constructor() {
     super(...arguments);
+
+    if(Meteor.user() != null)
+    {
+      this.name = Meteor.user().profile;
+    }
+    
+  }
+
+  logout() {
+    Meteor.logout((err) => {
+      if (err) return this.handleError(err);
+      this.$state.go('tab.login');
+    })
   }
 }
  
