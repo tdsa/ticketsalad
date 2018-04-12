058017786d329d5d0159595789d7b5765a41f9de
Tristan Joseph <tristan@Tristans-MacBook-Pro-2.local>
2018-04-12 19:12:07 +0200

    Tristans work on authentication, some stuff working

--- Application/ticketsalad/client/scripts/controllers/signup.controller.js
+++ Application/ticketsalad/client/scripts/controllers/signup.controller.js
@@ -2,9 +2,28 @@ import { _ } from 'meteor/underscore';
 import { Controller } from 'angular-ecmascript/module-helpers';
  
 export default class SignupCtrl extends Controller {
-  constructor() {
-    super(...arguments);
+  
+  signup() {
+    if (_.isEmpty(this.email)) return;
+    if (_.isEmpty(this.name)) return;
+    if (_.isEmpty(this.user)) return;
+    if (_.isEmpty(this.pass)) return;
+ 
+      Accounts.createUser({
+        username: this.user,
+        email: this.email,
+        password: this.pass,
+        profile: this.name
+      });
+
+      this.$state.go('tab.login', {
+        username: this.user,
+        email: this.email,
+        password: this.pass,
+        profile: this.name
+      });
   }
+
 }
  
 SignupCtrl.$name = 'SignupCtrl';
