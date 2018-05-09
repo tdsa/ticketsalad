4ae7854de1aafe2d6da2b40ad8073ba7053a4009
Tristan Joseph <tristan@Tristans-MacBook-Pro-2.local>
2018-05-09 21:04:51 +0200

    Credits

--- Application/ticketsalad/client/scripts/controllers/buyCredits.controller.js
+++ Application/ticketsalad/client/scripts/controllers/buyCredits.controller.js
@@ -13,11 +13,21 @@ import { Controller } from 'angular-ecmascript/module-helpers';
  
 export default class BuyCreditsCtrl extends Controller {
 
+  constructor()
+  {
+    super(...arguments);
+    $('#fullpage').fullpage({
+      verticalCentered: false,
+      crollOverflow: false
+  });
+  }
+
   done() //Takes a user back to their profile page
   {
     this.$state.go('tab.profile');
+      
   }
-}
+  }
  
 BuyCreditsCtrl.$name = 'BuyCreditsCtrl';
 BuyCreditsCtrl.$inject = ['$state', '$ionicPopup', '$log'];
