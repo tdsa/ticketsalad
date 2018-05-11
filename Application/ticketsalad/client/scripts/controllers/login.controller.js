8d9f479e165cc6ebd19e4683c23e11b98e1c2073
Thomas Honiball <honiballthomas@gmail.com>
2018-05-11 08:52:23 +0200

    Implemented claiming and fixed logout bug

--- Application/ticketsalad/client/scripts/controllers/login.controller.js
+++ Application/ticketsalad/client/scripts/controllers/login.controller.js
@@ -1,11 +1,60 @@
+/*
+* File Name: login.controller.js
+* Version 1.0
+*
+* Tribus Digita
+* Ticket Salad
+*
+* Functional description: login controller handles all javascript associated with the login html file. 
+all javascript functions along with the state controllers are placed here. login also handles the process of correctly 
+logging a user into the app and letting all other views show.
+
+*/
 import { _ } from 'meteor/underscore';
 import { Controller } from 'angular-ecmascript/module-helpers';
  
-export default class LoginCtrl extends Controller {
-  constructor() {
-    super(...arguments);
+export default class LoginCtrl extends Controller 
+{
+
+  
+  
+  login() {
+    if (_.isEmpty(this.email))
+    {
+      return;
+    }
+    if (_.isEmpty(this.pass)) return;
+ 
+    Meteor.loginWithPassword(this.email, this.pass, function (err) {
+      if (!err) {
+          console.log('I was called because authentication was a success');
+          return;
+      } else {
+          console.log(err);
+      }
+      })
+
+      if(Meteor.user())
+      {
+        this.view();
+      }     
+  }
+
+  view()
+  {
+    this.$state.go('tab.events');
+  }
+
+  create()
+  {
+    this.$state.go('signup');
+  }
+
+  forgot()
+  {
+    this.$state.go('forgotPassword');
   }
 }
  
-LoginCtrl.$name = 'LoginCtrl';
-LoginCtrl.$inject = ['$state', '$ionicPopup', '$log'];
+LoginCtrl.$name = 'LoginCtrl';//To refer to the controller in scope
+LoginCtrl.$inject = ['$state', '$ionicPopup', '$log'];// Adds the controller to the routes config
--- Application/ticketsalad/client/scripts/routes.js
+++ Application/ticketsalad/client/scripts/routes.js
@@ -1,10 +1,26 @@
+/*
+* File Name: routes.js
+* Version 1.0
+*
+* Tribus Digita
+* Ticket Salad
+*
+* Functional description: routes handles all javascript associated importing templates from the HTML counterparts, and configuring them as states
+These states are used to provide the different views of the application
+*/
+
+//libs
 import { _ } from 'meteor/underscore';
 import { Config, Runner } from 'angular-ecmascript/module-helpers';
- 
+
+//Templates
 import eventsTemplateUrl from '../templates/events.html';
 import profileTemplateUrl from '../templates/profile.html';
+import buyCreditsTemplateUrl from '../templates/buyCredits.html';
+import editProfileTemplateUrl from '../templates/editProfile.html';
 import loginTemplateUrl from '../templates/login.html';
 import signupTemplateUrl from '../templates/signup.html';
+import forgotPasswordTemplateUrl from '../templates/forgotPassword.html';
 import tabsTemplateUrl from '../templates/tabs.html';
  
 class RoutesConfig extends Config {
@@ -14,12 +30,28 @@ class RoutesConfig extends Config {
     super(...arguments);
   }
 
-  configure() {
+  configure() //Creates the templates as states
+  {
     this.$stateProvider
       .state('tab', {
         url: '/tab',
         abstract: true,
         templateUrl: tabsTemplateUrl
+      })
+      .state('forgotPassword', {
+        url: '/forgotpassword',
+        templateUrl: forgotPasswordTemplateUrl,
+        controller: 'ForgotPasswordCtrl as forgotPassword' 
+      })
+      .state('login', {
+        url: '/login',
+        templateUrl: loginTemplateUrl,
+        controller: 'LoginCtrl as login'
+      })
+      .state('signup', {
+        url: '/signup',
+        templateUrl: signupTemplateUrl,
+        controller: 'SignupCtrl as signup' 
       })
       .state('tab.events', {
         url: '/events',
@@ -40,32 +72,22 @@ class RoutesConfig extends Config {
         }
         
       })
-      .state('tab.login', {
-        url: '/login',
-        views: {
-          'tab-login': {
-            templateUrl: loginTemplateUrl,
-            controller: 'LoginCtrl as login'
-          }
-        }
-        
+      .state('buyCredits', {
+        url: '/buyCredits',
+        templateUrl: buyCreditsTemplateUrl,
+        controller: 'BuyCreditsCtrl as buyCredits',
       })
-      .state('tab.signup', {
-        url: '/signup',
-        views: {
-          'tab-signup': {
-            templateUrl: signupTemplateUrl,
-            controller: 'SignupCtrl as signup'
-          }
-        }
-        
+      .state('editProfile', {
+        url: '/editProfile',
+        templateUrl: editProfileTemplateUrl,
+        controller: 'EditProfileCtrl as editProfile'
       });
  
-    this.$urlRouterProvider.otherwise('tab/events');
+    this.$urlRouterProvider.otherwise('login'); //Default state
   }
 }
  
-RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider']; //Puts all the states in a router
  
 class RoutesRunner extends Runner {
   run() {
