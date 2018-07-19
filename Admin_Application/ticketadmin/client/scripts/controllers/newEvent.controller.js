2259dbea139036039a1abaffe9e404e0e9205e88
Thomas Honiball <honiballthomas@gmail.com>
2018-07-19 12:01:23 +0200

    Created working admin panel

---
+++ Admin_Application/ticketadmin/client/scripts/controllers/newEvent.controller.js
@@ -0,0 +1,53 @@
+import Moment from 'moment';
+import { Controller } from 'angular-ecmascript/module-helpers';
+import { Events } from '../../../lib/collections';
+
+export default class NewEventCtrl extends Controller {
+  constructor() {
+    super(...arguments);
+
+    this.helpers({
+      data() {
+        return Events.find();
+      }
+    });
+
+  }
+
+  generateCode()
+  {
+      var code = "";
+      for (var index = 0; index < 5; index++) 
+      {
+          var digit = Math.floor((Math.random() * 10));
+          code += digit + "";
+      }
+      //Console.log(code);
+      return code;
+  }
+
+  done()
+  {
+    var event = {
+      name: this.name,
+      city: this.city,
+      country: this.country,
+      picture: 'img/TL.jpg',
+      from: this.from,
+      to: this.to,
+      webpage: this.webpage,
+      credits: this.credits,
+      description: this.description,
+      claims: 0,
+      code: this.generateCode(),
+      claimed: 0,
+      winner: null
+    };
+
+    Events.insert(event);
+    this.$state.go("tab.events");
+  }
+}
+
+NewEventCtrl.$name = 'NewEventCtrl';
+NewEventCtrl.$inject = ['$state', '$ionicPopup', '$log'];
---
+++ Admin_Application/ticketadmin/server/bootstrap.js
@@ -0,0 +1,82 @@
+/*
+* File Name: bootstrap.js
+* Version 1.0
+*
+* Tribus Digita
+* Ticket Salad
+*
+* Functional description: bootstrap handles all javascript associated with creating a mongo collection
+*/
+
+//libs
+import Moment from 'moment';
+import { Meteor } from 'meteor/meteor';
+import { Events } from '../lib/collections';
+ 
+Meteor.startup(function() 
+{
+
+    function generateCode()
+    {
+        var code = "";
+        for (var index = 0; index < 5; index++) 
+        {
+            var digit = Math.floor((Math.random() * 10));
+            code += digit + "";
+        }
+        //Console.log(code);
+        return code;
+    }
+    Events.remove({}); //clears the collection
+
+    const events = [
+    {
+        name: 'Tomorrowland',
+        city: 'Boom',
+        country: 'Belgium',
+        picture: 'img/TL.jpg',
+        from: 'Jul 21, 2018',
+        to: 'Jul 29, 2018',
+        webpage: 'https://www.tomorrowland.com/en/festival/welcome',
+        credits: '15',
+        description: '',
+        claims: 75,
+        code: generateCode(),
+        claimed: 0,
+        winner: null
+    },
+    {
+        name: 'Rocking The Daisies',
+        city: 'New York',
+        country: 'America',
+        picture: 'img/RTD.jpg',
+        from: 'Oct 5, 2018',
+        to: 'Oct 7, 2018',
+        webpage: 'https://rockingthedaisies.com/',
+        credits: '5',
+        description: '',
+        claims: 52,
+        code: generateCode(),
+        claimed: 0,
+        winner: null
+    },
+    {
+        name: 'In The City',
+        city: 'Moscow',
+        country: 'Russia',
+        picture: 'img/ITC.png',
+        from: 'Oct 7, 2018',
+        to: 'Oct 9, 2018',
+        webpage: 'http://inthecityjhb.co.za/',
+        credits: '5',
+        description: '',
+        claims: 35,
+        code: generateCode(),
+        claimed: 0,
+        winner: null
+    }];
+
+    events.forEach((event) => {
+        const eventId = Events.insert(event); //Inserts into collections
+    });
+});
