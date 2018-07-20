c76094c93a1233c71d732416125977981a45dda3
Tristan Joseph <tristan@Tristans-MacBook-Pro-2.local>
2018-07-20 12:04:18 +0200

    Buy claims new UI

--- Application/ticketsalad/client/scripts/controllers/buyCredits.controller.js
+++ Application/ticketsalad/client/scripts/controllers/buyCredits.controller.js
@@ -16,11 +16,54 @@ export default class BuyCreditsCtrl extends Controller {
     constructor() 
     {
       super(...arguments);
+
+      this.helpers({
+        getUser()
+        {
+          console.log("Current loaded");
+          console.log(this.user);
+          console.log("Current logged");
+          console.log(Meteor.user());
+          this.user = Meteor.user();
+          console.log("New loaded");
+          console.log(this.user);
+          
+        }
+      });
+
+      this.amount = 0;
     }
 
     exit()
     {
         this.$state.go('profile');
+    }
+
+    option(add)
+    {
+      console.log("Current amount: " + this.amount);
+      console.log("Amount to add: " + add);
+      console.log("Expected result: " + this.amount + add);
+      this.amount = this.amount + add;
+      console.log("Actual result: " + this.amount);
+    }
+
+    pay()
+    {
+      this.total = 2*this.amount;
+      var userClaims = this.user.profile.credits;
+      var userClaims = userClaims + this.amount;
+      Meteor.users.update(this.user._id, {$set: {"profile.credits": userClaims}});
+    }
+
+    minus()
+    {
+      this.amount = this.amount - 10;
+    }
+
+    add()
+    {
+      this.amount = this.amount + 10;
     }
   }
  
