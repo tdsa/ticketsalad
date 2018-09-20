82d3c77782b76aedb90fc2299a2921615c8fc824
Thomas Honiball <honiballthomas@gmail.com>
2018-09-20 14:23:18 +0200

    Rebase

--- Testing/tests/events_page.js
+++
@@ -1,8 +0,0 @@
-test = {
-    'Claim with no code' : function(browser)
-    {
-        browser
-            .click(".eventsClaimBtn")
-            .assert.containsText('.instruction', 'Enter a code to claim!')
-    }
-};
